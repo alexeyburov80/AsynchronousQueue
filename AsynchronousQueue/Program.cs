a3f2df703af16af986f17402587d2c625defe808
alexeyburov <alexeyburov80@gmail.com>
2026-06-26 15:30:40 +0400

    add web UI

--- AsynchronousQueue/Program.cs
+++ AsynchronousQueue/Program.cs
@@ -78,6 +78,7 @@ using (var scope = app.Services.CreateScope())
 if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 
+app.UseDefaultFiles();
 app.UseStaticFiles();
 
 // ── Endpoints ─────────────────────────────────────────────────────────────────
